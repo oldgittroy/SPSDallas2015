1413dd8e1fb89c857ccbef4f275708a8137d9648
oldgittroy <troydune@gmail.com>
2015-01-27 10:11:30 -0600

    AngularProfile

---
+++ AngularProfile/Scripts/app/ProfileService.js
@@ -0,0 +1,40 @@
+﻿var profileModule = angular.module("ProfileServiceModule", []);
+
+profileModule.factory("ProfileREST", ["$http","UrlService", function($http, UrlService){
+  var  Profile = {};
+
+ Profile.query = function(){
+	 
+	 var baseUrl = UrlService.getBaseUrl();
+	 var restUrl = baseUrl + "/_api/SP.UserProfiles.PeopleManager/GetMyProperties";
+	 
+		return $http.get(restUrl);	
+	 
+    }//query()
+    
+return Profile;
+
+}//function($http)
+
+
+]); //.factory([
+
+
+profileModule.factory("ProfileResource", ["$resource", function($resource){
+  var  Profile = {};
+
+ Profile.query = function(){
+	 
+	 var baseUrl = UrlService.getBaseUrl();
+	 var restUrl = baseUrl + "/_api/SP.UserProfiles.PeopleManager/GetMyProperties";
+	 
+		return $resource(restUrl);	
+	 
+    }//query()
+    
+return Profile;
+
+}//function($http)
+
+
+]); //.factory([
